0eeacff88f5c2d83266b6394cdd0ac7d3aa0bb0f
Raphael Peters <rappet@rappet.de>
2019-03-21 15:02:12 +0100

    Updated version.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 extern crate bytes;
-#[macro_use]
 extern crate futures;
 extern crate tokio_io;
+extern crate tokio_codec;
 extern crate tokio_core;
 #[macro_use]
 extern crate error_chain;
@@ -10,10 +10,8 @@ pub mod error;
 pub mod pixel;
 pub mod command;
 pub mod codec;
-pub mod client;
 
 pub use error::Error;
 pub use pixel::{Pixel, Color, Coordinate};
 pub use command::{Command, Response};
 pub use codec::{PixelflutClientCodec, PixelflutServerCodec};
-pub use client::Client;
