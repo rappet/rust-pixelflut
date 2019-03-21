096b48cce7cddbc132bd33d641c10a942d48bf6c
Raphael Peters <rappet@rappet.de>
2019-03-21 23:19:14 +0100

    Added async feature.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,23 @@
+#[cfg(feature = "async")]
 extern crate bytes;
+#[cfg(feature = "async")]
 extern crate futures;
+#[cfg(feature = "async")]
 extern crate tokio_io;
+#[cfg(feature = "async")]
 extern crate tokio_codec;
+#[cfg(feature = "async")]
 extern crate tokio_core;
 
 pub mod error;
 pub mod pixel;
 pub mod command;
+#[cfg(feature = "async")]
 pub mod codec;
 pub mod sync;
 
 pub use error::{Error, Result};
 pub use pixel::{Pixel, Color, Coordinate};
 pub use command::{Command, Response};
+#[cfg(feature = "async")]
 pub use codec::{PixelflutClientCodec, PixelflutServerCodec};
